f8e6cd8c40bac7df5cd5de09ab64a6f9be0e32b8
rebeks314 <rebeks314@E29658.edu.linkoping.se>
2015-04-13 10:50:13 +0200

    #1

---
+++ omOss.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Djurkostymer.com</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="omOss.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
+        <link rel="icon" href="img/gandalfhundutklipp.gif" type="img/x-icon">
+    </head>
+
+    <body>
+
+       <header>
+                  
+            <ul>
+                <li><a href="index.html">Butik</a></li>
+                <li><a href="omOss.php">Om oss</a></li>
+                <li><a href="loggain.html">Logga in</a></li>
+                
+            </ul>
+             
+            <div class="vänsterboxheader">
+              <a href="index.html">Djur-</a>
+                <a href="index.html">kostymer.com</a>
+
+            </div>
+                                                                      
+        </header>
+
+        <div id="wrapper">
+
+        <div id="nav">
+                
+                <button id="sök" type="button">Sök</button> 
+                <input id="mysearch" type="search"/>
+                
+                <div id="navproduktruta">
+                    <h3>Produkter</h3>
+                </div>
+                
+        <nav class="main-nav">
+            <ul>
+                  <li><a href="Vuxna.html">Vuxna</a>
+                   <ul>
+                        <li><a href="#">Kille</a></li>
+                         <li><a href="#">Tjej</a></li>
+                      </ul>
+                  </li>               
+                  <li><a href="Barn.html">Barn</a>
+                   <ul>
+                        <li><a href="#">Kille</a></li>
+                         <li><a href="#">Tjej</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="hund1.html">Hund</a></li>
+                   <li><a href="Katt.html">Katt</a></li>                  
+            </ul>
+        </nav>
+           
+         </div>
+            
+            <div id="sectionruta">
+                
+                <div id="omossram" class="shadow">
+                    
+                    <div id="omossliten">
+                        
+                        <h1>Om oss</h1>
+                        
+                        <p>Denna sajt är gjord som ett slut projekt i webbutveckling 1 och där av är Djurkostymer.com ett påhittat företag.</p>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                           Excepteur sint occaecat cupidatat non proident,
+                           sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        
+                    </div>
+                    
+                </div>
+
+            </div>
+
+            <div id="footer">
+
+                <div id="footerbox">
+
+                    <div class="footertrebox" >
+
+                        <h3>Information</h3>
+
+                        <ul>  
+                            <li><a href="omoss.html">Om oss</a></li>
+                            <li><a href="kontaktkund.html">Kontakt oss</a></li>
+                            <li><a href="kontaktkund.html">Kundservis</a></li>
+                        </ul> 
+                    </div>
+
+                    <div class="footertrebox" >
+
+                        <h3>Kontakt info</h3>
+
+                        <p>Telefon: 0701237564 </p>
+                        <p>Mail: hurrdurr@gmail.com </p>
+                        <p>Adress: hurrdurrgatan 79a </p>       
+
+                    </div>
+
+                    <div class="footertrebox1" >
+
+                        <h3>Min sida</h3>
+
+                        <ul>
+
+                            <li><a href="loggain.html">Logga in</a></li>
+                            <li><a href="loggain.html">Sign up</a></li>
+                            <li><a href="#">Hjälp</a></li>
+
+                        </ul>
+
+                    </div>
+
+
+
+                </div>
+            </div>            
+        </div>       
+
+    </body>
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
